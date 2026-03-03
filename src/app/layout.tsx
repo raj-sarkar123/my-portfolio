0596c92bd92158ce2f6a3221b2c6ae85ab845bb3
Raj Sarkar <131342835+raj-sarkar123@users.noreply.github.com>
2026-03-03 18:00:59 +0530

    Removed Rocket branding scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,10 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  title: 'Next.js with Tailwind CSS',
-  description: 'A boilerplate project with Next.js and Tailwind CSS',
+  title: 'Raj Sarkar | Full Stack Developer',
+  description: 'Portfolio of Raj Sarkar - MERN Stack Developer',
   icons: {
-    icon: [
-      { url: '/assets/images/app_logo.png', type: 'image/x-icon' }
-    ],
+    icon: [{ url: '/assets/images/Raj_sarkar.jpg', type: 'image/png' }],
   },
 };
 
@@ -24,10 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}
-
-        <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Frajportfol6519back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.17" />
-        <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" /></body>
+      <body>{children}</body>
     </html>
   );
 }
